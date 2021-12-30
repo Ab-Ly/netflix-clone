19841211ce246035ab1af48f4d7b8772d5283f3d
Ab-Ly <abdelalilyoussefi@icloud.com>
2021-12-30 19:37:15 +0100

    install & scss config

---
+++ src/components/Nav.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState } from "react";
+import "./Nav.scss";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+function Nav() {
+  const [navBlack, setNavBlack] = useState(false);
+  const transitionNav = () => {
+    window.scrollY > 100 ? setNavBlack(true) : setNavBlack(false);
+  };
+  useState(() => {
+    document.addEventListener("scroll", transitionNav);
+  });
+
+  console.log(navBlack);
+  /* scroll */
+  /* burger */
+  return (
+    <div className={`nav  ${navBlack && "nav--black"}`}>
+      <button className="nav__burger">
+        <MenuIcon />
+      </button>
+      <img src="./images/logonetflix.png" className="nav__logo" alt="Netflix" />
+      <nav className="nav__links">
+        <a href="/" className="nav__link">
+          Accueil
+        </a>
+        <a href="/" className="nav__link">
+          Series
+        </a>
+        <a href="/" className="nav__link">
+          Films
+        </a>
+        <div className="nav__action">
+          <a href="/" className="nav__action">
+            <SearchIcon />
+          </a>
+          <a href="/" className="nav__action">
+            Direct
+          </a>
+          <a href="/" className="nav__action">
+            <CardGiftcardIcon />
+          </a>
+          <a href="/" className="nav__action">
+            <NotificationsIcon />
+          </a>
+          <a href="/" className="nav__action">
+            <img src="./images/netflix-avatar.png" alt="" />
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
