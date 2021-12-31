feb189e1994a7ed984a5eaa6181df567bc493734
Ab-Ly <abdelalilyoussefi@icloud.com>
2021-12-31 17:42:26 +0100

    Footer Config "OK"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,60 @@
 import React from "react";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+
 import "./Footer.scss";
 function Footer() {
-  return <footer className="footer">Toto</footer>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__socials">
+          <a href="/" className="footer__social">
+            <FacebookIcon />
+          </a>
+          <a href="/" className="footer__social">
+            <InstagramIcon />
+          </a>
+          <a href="/" className="footer__social">
+            <TwitterIcon />
+          </a>
+          <a href="/" className="footer__social">
+            <YouTubeIcon />
+          </a>
+        </div>
+        <ul className="footer__links">
+          <li className="footer__link">
+            <a href="/">Footer Link</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Footer Link</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Footer Link</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Footer Link</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Footer Link</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Footer Link</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Footer Link</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Footer Link</a>
+          </li>
+        </ul>
+        <div className="footer__copy">
+          Netflix Clone - Tous Droits Réservés 2022
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
