eb988ab3620f153aeddaafd26176dd49e502c848
Ab-Ly <abdelalilyoussefi@icloud.com>
2022-01-01 15:09:27 +0100

    Row Config "OK"

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Row from "./components/Row";
+import requests from "./config/Requests";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Banner />
-      <Row />
+      <Row
+        title="Programmes Originaux Netflix"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isPoster={true}
+      />
+      <Row title="Tendance Actuelles" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Films d'Action" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Films Comedie" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Films d'Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Films Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaires" fetchUrl={requests.fetchDocumentaries} />
       {/* video */}
       {/* quick view */}
 
